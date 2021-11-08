66d84bf064bc6ce4af3a5061a3b2fc28d275a331
unknown <chesebrojared@gmail.com>
2021-11-08 15:44:25 -0800

    user routes outlined

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,2 +1,34 @@
 const router = require ('express').Router();
 const { User } = require('../../models');
+
+// GET USERS
+router.get('/', (req, res) => {
+
+});
+
+// GET ONE USER
+router.get('/:id', (req, req) => {
+
+});
+
+// POST ONE USER
+router.post('/', (req, res) => {
+
+});
+
+// LOGIN ROUTE
+router.post('/login', (req, res) => {
+
+});
+
+// UPDATE USER
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE USER
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
