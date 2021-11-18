191a3636d7277c191ac194466df74b6000ba4cdb
Cienna97 <cmortenson97@gmail.com>
2021-11-18 12:47:59 -0800

    add logout.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,8 +38,13 @@ router.get('/', (req, res) => {
 });
 
 // create login route
-// router.get('/login', (req,res) => {
+router.get('/login', (req,res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+res.render('login');
+});
 
-// })
 
 module.exports = router;
