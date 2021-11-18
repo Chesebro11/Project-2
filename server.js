5d926f7c5d6898a8615c5cd69d95684949941353
unknown <chesebrojared@gmail.com>
2021-11-17 20:02:56 -0800

    bug fix

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-const session = require('express-session');
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-    secret: 'Secret',
-    cookie: {},
-    resave: false,
-    saveUnintialized: true,
-    store: new SequelizeStore({
-        db: SequelizeStore
-    })
-};
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
