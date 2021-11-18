4d2c84a75ee14e0af45f830b4600ed3a8eaeee4f
Cienna97 <cmortenson97@gmail.com>
2021-11-17 17:55:59 -0800

    routes for login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -28,17 +28,7 @@ router.get('/:id', (req, res) => {
             // comment model
         ]
     })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+
 });
 
 //for login.handlebars
@@ -62,8 +52,35 @@ router.post('/', (req, res) => {
 });
 
 // LOGIN ROUTE
-// router.post('/login', (req, res) => {
-// });
+router.post('/login', (req, res) => { 
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'There is no user with email address'});
+            return;
+        }
+        
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Password is incorrect' });
+            return;
+          }
+      
+          req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+      
+            res.json({ user: dbUserData, message: 'Login in succesful' });
+          });
+        });
+      });
+      
+ 
 
 // UPDATE USER
 router.put('/:id', (req, res) => {
@@ -94,15 +111,13 @@ router.delete('/:id', (req, res) => {
         }
     })
     .then(dbUserData => {
-        if(!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username_id = dbUserData.username;
+          req.session.loggedIn = true;
+
+          res.json(dbUserData);
+      });
     })
 });
 
