6cbda539b424772149f793fa460da63bae36de15
Octavio Politron <octavio.politron@gmail.com>
2021-11-18 19:18:54 -0800

    heroku bug

--- db/db.sql
+++
@@ -1,5 +0,0 @@
-DROP IF EXISTS dorm_forum;
-
-CREATE DATABASE dorm_forum;
-
-USE dorm_forum;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,15 @@
-INSERT INTO users (user_name, email, user_pass, user_rmNum)
-VALUES
-("Jason", "Parse@Json.com", "NoneShallPass", 789);
+-- INSERT INTO users (user_name, email, user_pass, user_rmNum)
+-- VALUES
+-- ("Jason", "Parse@Json.com", "NoneShallPass", 789);
 
-INSERT INTO posts (post_id, post_content, post_date, user_id)
-VALUES
-(
-    1,
-    "If you're reading this I am glad.",
-    "11/20/3021",
-    "Jason"
+-- INSERT INTO posts (post_id, post_content, post_date, user_id)
+-- VALUES
+-- (
+--     1,
+--     "If you're reading this I am glad.",
+--     "11/20/3021",
+--     "Jason"
 
-);
+-- );
 
--- if there is an issue it might be with date
+-- -- if there is an issue it might be with date
